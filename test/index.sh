ea3d521335f4a41de7678ae0119931169286d4af
Jesse Tane <jesse.tane@gmail.com>
2014-11-29 03:10:04 -0500

    update tests to use new bin location

--- test/index.sh
+++ test/index.sh
@@ -4,20 +4,22 @@ set -e
 
 cd "$(dirname "$BASH_SOURCE")"
 
+tsync="../bin/tsync"
+
 # coffee-script
-../index.js -x js 'fixtures/**/*.coffee' 'coffee -c --stdio' actual
+$tsync -x js 'fixture/**/*.coffee' 'coffee -c --stdio' actual
 echo "✓ coffee-script"
 
 # html repair and minify
-../index.js 'fixtures/**/*.html' htmlmin actual
+$tsync 'fixture/**/*.html' htmlmin actual
 echo "✓ htmlmin"
 
 # jade + uglify
-../index.js -x js 'fixtures/**/*.jade' 'jade -cD' uglifyjs actual
+$tsync -x js 'fixture/**/*.jade' 'jade -cD' uglifyjs actual
 echo "✓ jade + uglify"
 
 # lessc
-../index.js --extension css 'fixtures/**/*.less' 'lessc -' actual
+$tsync --extension css 'fixture/**/*.less' 'lessc -' actual
 echo "✓ lessc"
 
 # diff actual against expected
