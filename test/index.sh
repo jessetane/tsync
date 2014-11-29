129ae56e41225a7baf9f00cefa0ba07d3a4a9045
Jesse Tane <jesse.tane@gmail.com>
2014-11-29 03:55:52 -0500

    fix typo in tests

--- test/index.sh
+++ test/index.sh
@@ -7,19 +7,19 @@ cd "$(dirname "$BASH_SOURCE")"
 tsync="../bin/tsync"
 
 # coffee-script
-$tsync -x js 'fixture/**/*.coffee' 'coffee -c --stdio' actual
+$tsync -x js 'fixtures/**/*.coffee' 'coffee -c --stdio' actual
 echo "✓ coffee-script"
 
 # html repair and minify
-$tsync 'fixture/**/*.html' htmlmin actual
+$tsync 'fixtures/**/*.html' htmlmin actual
 echo "✓ htmlmin"
 
 # jade + uglify
-$tsync -x js 'fixture/**/*.jade' 'jade -cD' uglifyjs actual
+$tsync -x js 'fixtures/**/*.jade' 'jade -cD' uglifyjs actual
 echo "✓ jade + uglify"
 
 # lessc
-$tsync --extension css 'fixture/**/*.less' 'lessc -' actual
+$tsync --extension css 'fixtures/**/*.less' 'lessc -' actual
 echo "✓ lessc"
 
 # diff actual against expected
